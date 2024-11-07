81b6f3548aa57a75d9fd095fdd0ca43a0fccb22f
htejadag <htejadag25@gmail.com>
2024-11-06 21:19:43 -0500

    Initial commit

---
+++ MsSecurity/src/main/java/TDB2024II/MsSecurity/MsSecurityApplication.java
@@ -0,0 +1,13 @@
+package TDB2024II.MsSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSecurityApplication.class, args);
+	}
+
+}
